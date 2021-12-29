36cb02029c5c63fa1e6c3bfe59047105b9b3bb81
navneet <mani@indovations.in>
2021-12-29 06:57:31 +0530

    yesterday

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import Head from 'next/head';
+import useStyles from '../Utils/styles';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
 
 function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Shopping Point</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Shopping Point</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Navneet</Typography>
+      </footer>
     </div>
   );
 }
