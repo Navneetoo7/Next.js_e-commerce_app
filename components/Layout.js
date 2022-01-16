df60cb1bc657a98b7868f02bdc57e51119618aad
navneet <mani@indovations.in>
2022-01-17 02:49:22 +0530

    product

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import Head from 'next/head';
-import useStyles from '../Utils/styles';
-import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import useStyles from '../utils/styles';
+import NextLink from 'next/link';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+} from '@material-ui/core';
 
-function Layout({ children }) {
+function Layout({ title, children, description }) {
   const classes = useStyles();
   return (
     <div>
       <Head>
-        <title>Shopping Point</title>
+        <title>{title ? `${title} - Shopping Point` : 'Shopping Point'}</title>
+        {description && <meta name="description" content={description}></meta>}
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Shopping Point</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Shopping Point</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
