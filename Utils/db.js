2ba8b9ab25d88a1dd39173624a5e1a81943e0164
navneet <mani@indovations.in>
2022-01-22 22:52:13 +0530

    product data add in backend

--- Utils/db.js
+++ Utils/db.js
@@ -17,7 +17,7 @@ async function connect() {
   const db = await mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
   });
   console.log('new connection');
   connection.isConnected = db.connections[0].readyState;
